2aaadfcd20d75b07cdb6dca45c77104cb222249e
meiszhe1@163.com <TheFlash@flash.local>
2018-09-11 09:31:07 +0800

    1,初始化发布版本。

--- GitUseAgain/ViewController.m
+++ GitUseAgain/ViewController.m
@@ -10,15 +10,29 @@
 
 @interface ViewController ()
 
+@property (nonatomic, strong) UILabel *showLb;
+
+
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    [self createSubviews];
 }
 
+#pragma mark - 布局视图
+- (void)createSubviews
+{
+    self.showLb = [[UILabel alloc] initWithFrame:CGRectMake(100, 100, 250, 100)];
+    self.showLb.backgroundColor = [UIColor greenColor];
+    self.showLb.text = @"Release Version";
+    self.showLb.textColor = [UIColor redColor];
+    self.showLb.textAlignment = NSTextAlignmentCenter;
+    [self.view addSubview:self.showLb];
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
